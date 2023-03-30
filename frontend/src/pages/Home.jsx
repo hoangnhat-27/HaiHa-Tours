201da30bd6790a6a8f925845576ea4a60d751d30
hoangnhat-27 <hoangnhat2772001@gmail.com>
2023-03-31 01:15:55 +0700

    second commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,6 +12,8 @@ import SearchBar from "../shared/Searchbar";
 import ServiceList from "../services/ServiceList";
 import FeatureTourList from "../components/Featured-tours/FeatureTourList";
 import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
+import Testimonials from "../components/Testimonials/Testimonials";
+import Newsletter from "../shared/Newsletter";
 
 const Home = () => {
   return (
@@ -135,6 +137,22 @@ const Home = () => {
             <MasonryImagesGallery />
           </Col>
         </Container>
+      </section>
+
+      {/* testimonial start */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle="Fan love" />
+              <h2 className="testimonial__title">What our fans say about us</h2>
+            </Col>
+            <Col lg="12">
+              <Testimonials />
+            </Col>
+          </Row>
+        </Container>
+        <Newsletter />
       </section>
     </>
   );
